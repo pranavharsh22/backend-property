b420cb7b2c8e162ac8e8916582318c07029ff656
Pranav Kumar <pranavkumarrajput068@gmail.com>
2024-04-27 17:09:45 +0530

    first commit

---
+++ products.js
@@ -0,0 +1,88 @@
+const products = [
+  {
+    id: 1,
+    name: "MUNNAR, INDIA",
+    desc: "2BHK FLAT",
+    price: 20000,
+    beds:2,
+    rating:4.5,
+  
+    image:
+      "https://res.cloudinary.com/daoyqwbmi/image/upload/v1713873059/jace-afsoon-3-_02iJ_-zA-unsplash_meopsw.jpg",
+      
+  },
+  {
+    id: 2,
+    name: "NEW DELHI, INDIA ",
+    price: 18000,
+    desc: "1 BHK FLAT",
+    beds:1,
+    rating:3.5,
+   
+    image:
+      "https://res.cloudinary.com/daoyqwbmi/image/upload/v1713873021/adam-winger-5zX1KAjPl4o-unsplash_a7qara.jpg",
+  },
+  {
+    id: 3,
+    name: "MUMBAI, INDIA",
+    price: 30000,
+    image:
+      "https://res.cloudinary.com/daoyqwbmi/image/upload/v1713872901/chuttersnap-tvHsxfMp8Gw-unsplash_rjlgal.jpg",
+    desc: "3BHK FLAT",
+    beds:3,
+    rating:4.0,
+  },
+  {
+    id: 4,
+    name: "CHANDIGARH, INDIA",
+    price: 40000,
+    image:
+      "https://res.cloudinary.com/daoyqwbmi/image/upload/v1713872854/adam-winger-PCDlE94JjcI-unsplash_qijixe.jpg",
+    desc: "4BHK FLAT",
+    beds:4,
+    rating:5.0,
+  },
+  {
+    id: 5,
+    name: "ASSAM, INDIA",
+    price: 10000,
+    image:
+      "https://res.cloudinary.com/daoyqwbmi/image/upload/v1713876678/adam-winger-xgeE__AAWaQ-unsplash_k2mznk.jpg",
+    desc: "1BHK FLAT",
+    beds:1,
+    rating:3.5,
+  },
+  {
+    id: 6,
+    name: "KOLKATA, INDIA",
+    price: 15000,
+    image:'https://res.cloudinary.com/daoyqwbmi/image/upload/v1713878355/serge-esteve-zSpc1dmmWPA-unsplash_nswlmh.jpg',
+        desc: "2BHK FLAT",
+        beds:2,
+        rating:2.5,
+
+  },
+  {
+    id: 7,
+    name: "GUWAHATI,INDIA",
+    price: 25000,
+    value:'Galaxy S23 ultra',
+    image:
+      "https://res.cloudinary.com/daoyqwbmi/image/upload/v1713877096/crew-XenapSlQVmI-unsplash_bjpe7w.jpg",
+      desc:'3BHK FLATS',
+      beds:3,
+      rating:4.0,
+  },
+  {
+    id: 8,
+    name: "SHIMLA, INDIA",
+    price: 30000,
+    
+    image:
+      "https://res.cloudinary.com/daoyqwbmi/image/upload/v1713877136/francesca-saraco-_dS27XGgRyQ-unsplash_ovxidn.jpg",
+    desc: "4BHK FLAT",
+    beds:4,
+    rating:1.5,
+  },
+];
+module.exports = products;
